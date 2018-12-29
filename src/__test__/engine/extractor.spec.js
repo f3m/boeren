b70794af2bbe013e5afbfe5eb966051b5d5e5de2
ian-mcwilliams <ian.mcwilliams@f3mmedia.com>
2018-12-29 18:32:56 +0100

    return subset of results when final payment date is specified

--- src/__test__/engine/extractor.spec.js
+++ src/__test__/engine/extractor.spec.js
@@ -2,15 +2,27 @@ import Engine from "../../engine/engine";
 import { Extractor } from "../../engine/extractor";
 import { createFrequencyExtractor } from "../../engine/frequency-extractor";
 import exampleTransactions from "./example-extractors";
+const moment = require("moment");
 
 describe("the Extractor class", () => {
-  let engine = new Engine(exampleTransactions.monthly);
-  let monthlyExtractor = createFrequencyExtractor("monthly", new Date());
-  engine.addExtractor(monthlyExtractor);
-
   it("returns 12 rows when given one monthly row", () => {
+    let engine = new Engine(exampleTransactions.monthly);
+    let monthlyExtractor = createFrequencyExtractor("monthly", new Date());
+    engine.addExtractor(monthlyExtractor);
     let result = engine.run();
     expect(result.length).toEqual(12);
+  });
+
+  it("returns less than 12 rows when given one monthly row with a final payment field", () => {
+    let monthlyTransactions = exampleTransactions.monthly;
+    monthlyTransactions[0].finalPayment = JSON.stringify(
+      moment(new Date()).add(8, "month")
+    ).substr(1, 10);
+    let engine = new Engine(monthlyTransactions);
+    let monthlyExtractor = createFrequencyExtractor("monthly", new Date());
+    engine.addExtractor(monthlyExtractor);
+    let result = engine.run();
+    expect(result.length).toEqual(8);
   });
 
   it("returns true when the rawRow's criteria matches the trigger value", () => {
--- src/engine/frequency-extractor.js
+++ src/engine/frequency-extractor.js
@@ -17,21 +17,33 @@ class DefaultFrequencyExtractor extends Extractor {
     return null;
   };
 
+  //todo override this in the weekl and other extractors or keep it emptz here
   getFirstPaymentDate = row => {
     let dayString = row["payment_date"];
-    let day = !dayString ? 1 : Number(dayString.match("d+"));
-    return moment(this.startDate).date(day);
+    let day = !dayString ? 1 : Number(dayString.match(/\d+/)[0]);
+    return moment(this.startDate)
+      .date(day)
+      .toDate();
   };
 
   transform(parameters, rawRow, startDate, allRows, transformedRows) {
-    let firstPaymentDate = this.getFirstPaymentDate(rawRow);
-    let dates = this.getDateSeries(this.paymentDate, this.numberOfRepeats);
+    let firstPaymentDateStr = JSON.stringify(
+      this.getFirstPaymentDate(rawRow)
+    ).substr(1, 10);
+    let firstPaymentDate = new Date(firstPaymentDateStr);
+    let finalPaymentStr = rawRow["finalPayment"];
+    let finalPaymentDate = finalPaymentStr
+      ? new Date(finalPaymentStr)
+      : undefined;
+    let dates = this.getDateSeries(firstPaymentDate, this.numberOfRepeats);
     if (dates != null) {
-      return dates.next(this.numberOfRepeats).map(date => {
-        let newRow = { ...rawRow };
-        newRow["payment_date"] = date;
-        return newRow;
-      });
+      return dates
+        .filter(date => !finalPaymentDate || date.toDate() <= finalPaymentDate)
+        .map(date => {
+          let newRow = { ...rawRow };
+          newRow["payment_date"] = date;
+          return newRow;
+        });
     }
     return null;
   }
@@ -44,40 +56,44 @@ export const createFrequencyExtractor = (frequency, startDate) => {
         frequency,
         52,
         (paymentDate, numberOf) =>
-          moment(startDate)
+          moment(paymentDate)
             .recur()
             .every(1)
             .weeks()
+            .next(numberOf)
       );
     case "monthly":
       return new DefaultFrequencyExtractor(
         frequency,
         12,
         (paymentDate, numberOf) =>
-          moment(startDate)
+          moment(paymentDate)
             .recur()
             .every(1)
             .months()
+            .next(numberOf)
       );
     case "quarterly":
       return new DefaultFrequencyExtractor(
         frequency,
         4,
         (paymentDate, numberOf) =>
-          moment(startDate)
+          moment(paymentDate)
             .recur()
             .every(3)
             .months()
+            .next(numberOf)
       );
     case "annual":
       return new DefaultFrequencyExtractor(
         frequency,
         1,
         (paymentDate, numberOf) =>
-          moment(startDate)
+          moment(paymentDate)
             .recur()
             .every(1)
             .year()
+            .next(numberOf)
       );
     case "one-off":
       return new DefaultFrequencyExtractor(
