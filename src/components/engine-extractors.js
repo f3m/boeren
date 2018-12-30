6b4cf75f28ba31cd8d33502f38c5ae5fd1f47e76
Werner <werner@v-change.co.uk>
2018-12-30 01:02:04 +0100

    Added enabling in principle to the component

--- src/components/engine-extractors.js
+++ src/components/engine-extractors.js
@@ -2,21 +2,27 @@ import React, { Component } from "react";
 import DefaultEngine from "../engine/default-engine";
 import rawTransactions from "../artefacts/raw-transactions.json";
 
-const engine = new DefaultEngine(rawTransactions);
-
 export class EngineExtractors extends Component {
+  engine = new DefaultEngine([]);
+
+  handleExtractorToggle = extractor => {
+    extractor.enabled = !extractor.enabled;
+    // this.setState({engine:this.state.engine});
+  };
+
   render() {
     return (
       <div className="EngineExtractors">
-        <h1>{engine.engineName}: Extractors</h1>
+        <h1>{this.engine.engineName}: Extractors</h1>
         <ul>
-          {engine.extractors.map(extractor => (
+          {this.engine.extractors.map(extractor => (
             <li key={`li_${extractor.name}`}>
               <input
                 key={`input_${extractor.name}`}
                 type="checkbox"
                 value="true"
-                selected
+                checked="{extractor.enabled}"
+                onClick={this.handleExtractorToggle(extractor)}
               />
               <div key={extractor.name}>{extractor.name}</div>
             </li>
--- src/engine/engine.js
+++ src/engine/engine.js
@@ -14,19 +14,21 @@ export default class Engine {
     this.rawTransactions.forEach(row => {
       let result = [];
       console.log("=> Processing row:" + row);
-      this.extractors.forEach(extractor => {
-        console.log("==> Applying extractor:" + extractor);
-        if (extractor.applies(row)) {
-          result = extractor.transform(
-            this.parameters,
-            row,
-            startDate,
-            this.rawTransactions,
-            this.allResults
-          );
-          result.forEach(row => this.extractedTransactions.push(row));
-        }
-      });
+      this.extractors
+        .filter(ex => ex.enabled)
+        .forEach(extractor => {
+          console.log("==> Applying extractor:" + extractor);
+          if (extractor.applies(row)) {
+            result = extractor.transform(
+              this.parameters,
+              row,
+              startDate,
+              this.rawTransactions,
+              this.allResults
+            );
+            result.forEach(row => this.extractedTransactions.push(row));
+          }
+        });
     });
     // console.log(this.extractedTransactions);
     return this.extractedTransactions;
